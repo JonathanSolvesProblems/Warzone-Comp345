72e2e5a620f45a23a51dbcc0edf566753e4861e0
Drew Wagner <drewwagner@DESKTOP-HUVKRQQ.localdomain>
2020-10-11 22:42:12 -0400

    documented builder

--- buildMapDriver.sh
+++ buildMapDriver.sh
@@ -1,3 +1,10 @@
+echo Creating build/MapDriver Directory
 mkdir -p build/MapDriver
+echo Compiling Map.cpp to object file
 g++ -I ./headers -o build/MapDriver/Map.o -c src/Map.cpp
+echo Compiling and Linking MapDriver.cpp and Map.o
 g++ -I ./headers -o MapDriver drivers/MapDriver.cpp build/MapDriver/Map.o
+echo Cleaning up...
+rm build/MapDriver/*.o
+rmdir build/MapDriver
+echo Done.
