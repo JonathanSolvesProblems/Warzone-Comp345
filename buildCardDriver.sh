0a27ea5321767fcd51545f3bcd670282c879eddf
JonathanSolvesProblems <jon_andrei04@hotmail.com>
2020-10-12 20:56:53 -0400

    made changes to cards.

--- buildCardDriver.sh
+++ buildCardDriver.sh
@@ -3,9 +3,10 @@ mkdir -p build/CardDriver
 
 echo Compiling Card.cpp to object file
 g++ -I ./headers -o build/CardDriver/Card.o -c src/Card.cpp
+g++ -I ./headers -o build/CardDriver/Orders.o -c src/Orders.cpp
 
 echo Compiling and Linking CardDriver.cpp and Card.o
-g++ -I ./headers -o CardDriver drivers/CardDriver.cpp build/CardDriver/Card.o
+g++ -I ./headers -o CardDriver drivers/CardDriver.cpp build/CardDriver/Card.o build/CardDriver/Orders.o
 
 echo Cleaning up...
 rm build/CardDriver/*.o
