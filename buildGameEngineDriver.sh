583c4bfa61f864038f88c4870c553868c7ec180e
Drew Wagner <drewwagner@DESKTOP-HUVKRQQ.localdomain>
2020-11-01 13:25:08 -0500

    build script

---
+++ buildGameEngineDriver.sh
@@ -0,0 +1,15 @@
+echo Creating build/GameEngineDriver Directory
+mkdir -p build/GameEngineDriver
+
+echo Compiling to objects file
+g++ -I ./headers -o build/GameEngineDriver/Observers.o -c src/Observers.cpp
+g++ -I ./headers -o build/GameEngineDriver/GameEngine.o -c src/GameEngine.cpp
+
+echo Compiling and Linking GameEngineDriver.cpp with objects files.
+g++ -I ./headers -o GameEngineDriver drivers/GameEngineDriver.cpp build/GameEngineDriver/*.o -lcurses
+
+echo Cleaning up...
+rm build/GameEngineDriver/*.o
+rmdir build/GameEngineDriver
+
+echo Done.
