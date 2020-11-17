5f7c6a1299c4f1eab14a28d05b2323cbd26e5005
Drew Wagner <drewwagner@DESKTOP-HUVKRQQ.localdomain>
2020-11-16 21:10:07 -0500

    typo

--- buildGameEngineDriver.bat
+++ buildGameEngineDriver.bat
@@ -5,12 +5,10 @@ g++ -I .\headers -o build\GameEngineDriver\Player.o -c src\Player.cpp
 g++ -I .\headers -o build\GameEngineDriver\Card.o -c src\Card.cpp
 g++ -I .\headers -o build\GameEngineDriver\Orders.o -c src\Orders.cpp
 
-g++ -I .\headers -o build\GameEngineDriver\Observers.o -c src\Observers.cpp
 g++ -I .\headers -o build\GameEngineDriver\Application.o -c src\Application.cpp
 g++ -I .\headers -o build\GameEngineDriver\GameEngine.o -c src\GameEngine.cpp
 
-
-g++ -I .\headers -o build\GameEngineDriver\Observers.o -c src\GameObservers.cpp
+g++ -I .\headers -o build\GameEngineDriver\GameObservers.o -c src\GameObservers.cpp
 
 g++ -std=c++17 -I .\headers -o GameEngineDriver drivers\GameEngineDriver.cpp src\MapLoader.cpp build\GameEngineDriver\*.o -lpdcurses 
 
