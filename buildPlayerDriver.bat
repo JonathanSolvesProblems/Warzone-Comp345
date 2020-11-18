62f0ed0226a6c6da7e807cbffc4cfcc482f47120
JonathanSolvesProblems <jon_andrei04@hotmail.com>
2020-11-17 21:07:16 -0500

    "reverting merge."

--- buildPlayerDriver.bat
+++ buildPlayerDriver.bat
@@ -1,18 +1,17 @@
-echo Creating build\PlayerDriver Directory
 mkdir build\PlayerDriver
 
-echo Compiling to objects file
 g++ -I .\headers -o build\PlayerDriver\Map.o -c src\Map.cpp
 g++ -I .\headers -o build\PlayerDriver\Player.o -c src\Player.cpp
 g++ -I .\headers -o build\PlayerDriver\Card.o -c src\Card.cpp
 g++ -I .\headers -o build\PlayerDriver\Orders.o -c src\Orders.cpp
-g++ -I .\headers -o build\PlayerDriver\MapLoader.o -c src\MapLoader.cpp
 
-echo Compiling and Linking PlayerDriver.cpp with objects files.
-g++ -I .\headers -o PlayerDriver drivers\PlayerDriver.cpp build\PlayerDriver\*.o
+g++ -I .\headers -o build\PlayerDriver\Application.o -c src\Application.cpp
+g++ -I .\headers -o build\PlayerDriver\GameEngine.o -c src\GameEngine.cpp
 
-echo Cleaning up...
+g++ -I .\headers -o build\PlayerDriver\GameObservers.o -c src\GameObservers.cpp
+
+g++ -std=c++17 -I .\headers -o PlayerDriver drivers\PlayerDriver.cpp src\MapLoader.cpp build\PlayerDriver\*.o -lpdcurses -g
+
 del build\PlayerDriver\*.o
 rmdir build\PlayerDriver
-
-echo Done.
+pause
