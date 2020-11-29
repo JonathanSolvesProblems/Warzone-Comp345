9fe602b40d11ff56c861cfa9ea102f8e79d00131
Drew Wagner <drewwagner@DESKTOP-HUVKRQQ.localdomain>
2020-11-29 12:27:08 -0500

    fixed multiple definitions

--- src/GameEngine.cpp
+++ src/GameEngine.cpp
@@ -1,51 +1,6 @@
 #include "GameEngine.h"
 
 
-// checks the current phase of the game for N curses
-string convertEnum(Phase current_phase)
-{
-  // a switch statement that checks the current phase and sets it 
-  switch (current_phase)
-  {
-  case STARTUP:
-    return "Startup Phase";
-  case REINFORCEMENT:
-    return "Reinforcement Phase";
-  case ISSUE_ORDERS:
-    return "Issue Orders Phase";
-  case ORDERS_EXECUTION:
-    return "Orders Execution Phase";
-  default:
-    return "Unknown Phase";
-  }
-}
-
-// sets the observable message log
-GameModel::GameModel()
-{
-  log = new StringLog();
-  phase_headers_enabled = new ConcreteObservable<bool>();
-  stats_headers_enabled = new ConcreteObservable<bool>();
-  number_of_players = new ConcreteObservable<int>();
-
-  current_phase = new ConcreteObservable<Phase>();
-  current_player = new ConcreteObservable<Player*>();
-  active_players = new VectorObservable<Player *>();
-}
-
-// deletes the observable message log
-GameModel::~GameModel()
-{
-  delete log;
-  delete phase_headers_enabled;
-  delete stats_headers_enabled;
-  delete number_of_players;
-
-  delete current_phase;
-  delete current_player;
-  delete active_players;
-}
-
 //creates what we see in the main menu 
 MainMenuView::MainMenuView(int w, int h, GameModel *mgm) : WindowView(w, h, (COLS - w) / 2, (LINES - h) / 2)
 {
